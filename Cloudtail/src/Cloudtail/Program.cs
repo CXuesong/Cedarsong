715855997cf8198b8d7e865e08879cade382709a
forest93 <forest93@163.com>
2016-12-09 21:44:27 +0800

    Add skeleton for en Warriors Wiki tracking project.

---
+++ Cloudtail/src/Cloudtail/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WikiClientLibrary;
+using WikiClientLibrary.Client;
+using WikiDiffSummary;
+
+namespace Cloudtail
+{
+    public class Program
+    {
+
+        public const string EnApiEntryPoint = "http://warriors.wikia.com/api.php";
+        public const string ZhApiEntryPoint = "http://warriors.huiji.wiki/api.php";
+
+        public static void Main(string[] args)
+        {
+            RoutineAsync().Wait();
+        }
+
+        public static async Task RoutineAsync()
+        {
+            var client = new WikiClient();
+            var siteEn = await Site.CreateAsync(client, EnApiEntryPoint);
+            //var siteZh = await Site.CreateAsync(client, ZhApiEntryPoint);
+            var page = Page.FromTitle(siteEn, "Graystripe");
+            var revs = await page.EnumRevisionsAsync(2, PageQueryOptions.FetchContent).Take(2).ToList();
+            var cmp = new WikitextBySectionComparer();
+            var diff = cmp.Compare(revs[1].Content, revs[0].Content);
+            foreach (var d in diff)
+            {
+                if (d.Status != SectionDiffStatus.Identical)
+                {
+                    Console.WriteLine(d);
+                }
+            }
+        }
+    }
+}
---
+++ Cloudtail/tools/setvars.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+PATH %PATH%;%CD%
+CD ../lib
